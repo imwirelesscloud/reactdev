bbcdc4d041649b3a7634d0b5122bab1fffed3671
Manuel Lopez Tambriz <122247515+mlopezmaya@users.noreply.github.com>
2024-09-13 15:43:06 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="logo.png" className="App-logo" alt="logo" />
+        <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+           <span className="heart">
         </p>
         <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
+          
         </p>
         <p>
           <a
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            TERMINOLOGY II
           </a>
         </p>
       </header>
